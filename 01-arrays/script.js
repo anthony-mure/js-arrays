6951473548c076cfe0f236551c34f45834f566d7
anthony-murè <tony.mure99@gmail.com>
2025-07-03 18:43:42 +0200

    [fix] n-3

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -14,10 +14,10 @@ const fourthTeacher = teachers[3];
 console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers.splice(4,1,"Patrick");
+  teachers.splice(4,1,"Patrick");
 console.log(teachers);
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = teachers.splice(0,6);
+const lastTeacher = teachers.pop();
 console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
@@ -32,7 +32,8 @@ teachers.unshift("Sarah");
 console.log(teachers);
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf(5);
+console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
